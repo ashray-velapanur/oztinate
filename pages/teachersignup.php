8e4bfa2f105c205bf13fd0d2f0add6afec250939
ashray <ashray.velapanur@gmail.com>
2016-09-19 21:55:31 +0530

    moved html head to base php

--- pages/teachersignup.php
+++ pages/teachersignup.php
@@ -1,39 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Oztinate Admin panel</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href="../bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="../bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
+<?php include("base.php") ?>
 
 <body>
-
     <div class="container">
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
@@ -43,38 +13,29 @@
                     </div>
                     <div class="panel-body">
                         <form role="form" method="post" action="">
-                                <div class="form-group">
-                                    <label for="name">Name</label>
-                                    <input class="form-control" name="name" type="text" autofocus>
-                                </div>
-                                <div class="form-group">
-                                    <label for="email">Email</label>
-                                    <input class="form-control" name="email" type="text" autofocus>
-                                </div>
-                                <div class="form-group">
-                                    <label for="password">Password</label>
-                                    <input class="form-control" name="password" type="password" value="">
-                                </div>
-                                <input type="submit" value="Signup" class="btn btn-lg btn-success btn-block"/>
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input class="form-control" name="name" type="text" autofocus>
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input class="form-control" name="email" type="text" autofocus>
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input class="form-control" name="password" type="password" value="">
+                            </div>
+                            <input type="submit" value="Signup" class="btn btn-lg btn-success btn-block"/>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <!-- jQuery -->
     <script src="../bower_components/jquery/dist/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
     <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
     <script src="../bower_components/metisMenu/dist/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
     <script src="../dist/js/sb-admin-2.js"></script>
-
 </body>
 
 </html>
