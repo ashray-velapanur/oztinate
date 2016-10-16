140ccf344f9f0386165653a828db054d8f3b3c0d
ashray <ashray.velapanur@gmail.com>
2016-10-16 11:32:21 +0530

    fixed indentation

--- pages/teacher/nav.php
+++ pages/teacher/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-dark navbar-static-top">
     <div class="navbar-header">
-      <a class="navbar-brand" href="/oztinate_dev/teacher/home">Oztinate</a>
+        <a class="navbar-brand" href="/oztinate_dev/teacher/home">Oztinate</a>
     </div>
     <ul class="nav navbar-nav">
         <li class="nav-item">
@@ -10,7 +10,7 @@
             <a class="nav-link" href="/oztinate_dev/teacher/create_exercise">Exercises</a>
         </li>
         <li class="nav-item">
-              <a class="nav-link" href="/oztinate_dev/teacher/logout">Logout</a>
+            <a class="nav-link" href="/oztinate_dev/teacher/logout">Logout</a>
         </li>
     </ul>
 </nav>
