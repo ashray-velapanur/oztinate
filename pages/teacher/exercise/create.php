8df243900825f38bcfbdbf27ab401858c045540d
ashray <ashray.velapanur@gmail.com>
2016-10-17 14:19:23 +0530

    changed input type

--- pages/teacher/exercise/create.php
+++ pages/teacher/exercise/create.php
@@ -7,29 +7,29 @@
     <?php include(ROOT_DIR."/pages/teacher/nav.php") ?>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <h3 class="page-header">Create Exercise Template</h3>
-                        </div>
-                        <!-- /.col-lg-12 -->
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="h3 page-header">Create Exercise Template</div>
                     </div>
-                    <form role="form" method="post" action="">
+                </div>
+                <form role="form" method="post" action="">
                     <div class="form-group">
                         <label for="name">Name</label>
                         <input class="form-control" name="taskName" type="text" autofocus>
                     </div>
                     <div class="form-group">
                         <label for="email">Instruction</label>
-                        <input class="form-control" name="instruction" type="text" autofocus>
+                        <textarea class="form-control" name="instruction" type="text" autofocus></textarea>
                     </div>
                     <div class="form-group">
                         <label for="minimumDuration">Minimim Duration</label>
-                        <input class="form-control" name="minDuration" type="text" value="">
+                        <input class="form-control" name="minDuration" type="number" min="0" value="">
                     </div>
                     <div class="form-group">
                         <label for="practiceDuration">Practice Duration</label>
-                        <input class="form-control" name="practiceDuration" type="text" value="">
+                        <input class="form-control" name="practiceDuration" type="number" min="0" value="">
                     </div>
                     <div class="form-group">
                         <label for="details">Details</label>
@@ -38,6 +38,7 @@
                     <input type="submit" value="Create" class="btn btn-lg btn-success btn-block"/>
                 </form>
             </div>
+            <div class="col-md-4"></div>
         </div>
     </div>
     <script src="../bower_components/jquery/dist/jquery.min.js"></script>
