df9b14bbcad236a1ff68814bf1009fff281ca0be
ashray <ashray.velapanur@gmail.com>
2016-10-31 17:41:51 +0530

    renamed table name

--- src/tagCls.php
+++ src/tagCls.php
@@ -2,7 +2,7 @@
 class Tag{
 
 	public static function createTag($name) {
-		$query = "INSERT INTO tags (name) VALUES ('".$name."')";
+		$query = "INSERT INTO tag (name) VALUES ('".$name."')";
 		$result = mysql_query($query)or die(mysql_error());
 		if($result) 
 			return true;
