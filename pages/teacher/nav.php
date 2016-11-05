9d4673c1ab3279bedfc0b09a31f4bc37bc38dca3
ashray <ashray.velapanur@gmail.com>
2016-11-05 19:31:24 +0530

    fixed navbar

--- pages/teacher/nav.php
+++ pages/teacher/nav.php
@@ -1,26 +1,30 @@
-<nav class="navbar navbar-dark navbar-static-top row">
-    <div class="col-md-1">
-    </div>
-    <div class="col-md-10">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="/oztinate_dev/teacher/home">Oztinate</a>
+<nav class="navbar navbar-dark navbar-static-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-1">
+            </div>
+            <div class="col-md-10">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="/oztinate_dev/teacher/home">Oztinate</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/oztinate_dev/teacher/home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/oztinate_dev/teacher/exercises">Exercises</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/oztinate_dev/teacher/students">Students</a>
+                    </li>
+                </ul>
+                  <form class="navbar-form pull-right">
+                    <a href="/oztinate_dev/teacher/logout" type="submit" class="btn btn-primary">Logout</a>
+                  </form>
+                </ul>
+            </div>
+            <div class="col-md-1">
+            </div>
         </div>
-        <ul class="nav navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="/oztinate_dev/teacher/home">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/oztinate_dev/teacher/exercises">Exercises</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/oztinate_dev/teacher/students">Students</a>
-            </li>
-        </ul>
-          <form class="navbar-form pull-right">
-            <a href="/oztinate_dev/teacher/logout" type="submit" class="btn btn-primary">Logout</a>
-          </form>
-        </ul>
-    </div>
-    <div class="col-md-1">
     </div>
 </nav>
