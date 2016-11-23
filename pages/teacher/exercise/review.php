22753a37b4359551548c9dee30467212243fb374
ashray <ashray.velapanur@gmail.com>
2016-11-23 20:39:50 +0530

    updated review exercises page

--- pages/teacher/exercise/review.php
+++ pages/teacher/exercise/review.php
@@ -10,62 +10,68 @@
             <div class="col-md-4">
             </div>
             <div class="col-md-4">
-                <div class="row">
-                    <div class="col-md-12  page-header">
-                        <div class="h3 pull-left">Review Exercise</div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="h3">Review Exercise</div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group">
-                            <label for="clips">Clips</label>
-                            <div classs="clips">
-                                <?php foreach ($data["clips"] as $clip) { ?>
-                                    <audio src=<?php echo $clip ?> controls></audio>
-                                <?php } ?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group">
-                            <label for="comments">Comments</label>
-                            <div id="comments" name="comments">
-                                <?php foreach ($data["comments"] as $comment) { ?>
-                                <div class="panel">
-                                    <h5><?php echo $comment ?></h5>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="clips">Clips</label>
+                                    <div classs="clips">
+                                        <?php foreach ($data["clips"] as $clip) { ?>
+                                            <audio src=<?php echo $clip ?> controls></audio>
+                                        <?php } ?>
+                                    </div>
                                 </div>
-                                <?php } ?>
                             </div>
                         </div>
-                        <form role="form" method="post" action="/oztinate_dev/admin/addComment">
-                            <div class="form-group">
-                                <input class="hidden" name="assTaskId" id="assTaskId" value="<?php echo $data["task"]["Id"]; ?>">
-                                <input class="hidden" name="userId" id="userId" value="3">
-                                <textarea id="comment" name="comment" class="form-control" rows="3"></textarea>
-                                <button type="submit" class="btn btn-md btn-success pull-right">Add Comment</button>
-                                <!-- <input type="submit" value="Add Comment" class="btn btn-md btn-success pull-right"/> -->
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="comments">Comments</label>
+                                    <div id="comments" name="comments">
+                                        <?php foreach ($data["comments"] as $comment) { ?>
+                                        <div class="panel">
+                                            <h5><?php echo $comment ?></h5>
+                                        </div>
+                                        <?php } ?>
+                                    </div>
+                                </div>
+                                <form role="form" method="post" action="/oztinate_dev/admin/addComment">
+                                    <div class="form-group">
+                                        <input class="hidden" name="assTaskId" id="assTaskId" value="<?php echo $data["task"]["Id"]; ?>">
+                                        <input class="hidden" name="userId" id="userId" value="3">
+                                        <textarea id="comment" name="comment" class="form-control" rows="3"></textarea>
+                                        <!-- <input type="submit" value="Add Comment" class="btn btn-md btn-success pull-right"/> -->
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-md btn-success pull-right">Add Comment</button>
+                                    </div>
+                                </form>
                             </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <form role="form" method="post" action="">
-                            <div class="form-group">
-                                <label for="rating">Rating</label>
-                                <input class="hidden" name="assTaskId" id="assTaskId" value="<?php echo $data["task"]["Id"]; ?>">
-                                <select class="form-control" name="rating" id="rating">
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                </select>
-                                <button type="submit" class="btn btn-md btn-success pull-right">Rate</button>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <form role="form" method="post" action="">
+                                    <div class="form-group">
+                                        <label for="rating">Rating</label>
+                                        <input class="hidden" name="assTaskId" id="assTaskId" value="<?php echo $data["task"]["Id"]; ?>">
+                                        <select class="form-control" name="rating" id="rating">
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                            <option value="5">5</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-md btn-success pull-right">Rate</button>
+                                    </div>
+                                </form>
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
