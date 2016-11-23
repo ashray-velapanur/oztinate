bad035d14fea1ee4d7d1e00341d454878f1a6f3c
ashray <ashray.velapanur@gmail.com>
2016-11-23 21:11:37 +0530

    updated student details page

--- pages/teacher/student/details.php
+++ pages/teacher/student/details.php
@@ -7,109 +7,112 @@
     <?php include(ROOT_DIR."/pages/teacher/nav.php") ?>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-3">
-            </div>
+            <div class="col-md-3"></div>
             <div class="col-md-6">
-                <div class="row">
-                    <div class="col-md-9">
-                        <div class="h3 pull-left">Goals</div>
-                    </div>
-                    <div class="col-md-3">
-                        <form action="/oztinate_dev/teacher/goals">
-                            <input class="hidden" name="userId" id="userId" value="<?php echo $data["userId"]; ?>">
-                            <button type="submit" class="btn btn-primary btn-block">New Goal</button>
-                        </form>
-                    </div>
-                </div>
-                <?php foreach ($data["goals"] as $goal) {?>
-                <div class="row panel panel-default">
-                    <div class="panel-body">
-                        <div class="col-md-12">
-                            <div class="row">
-                                <div class="col-md-2">
-                                    <div class="h4 pull-left">
-                                        <span class="glyphicon glyphicon-tag" aria-hidden="true"></span>
-                                    </div>
-                                    <div class="h4 pull-left">
-                                        <?php echo $goal["name"]; ?>
-                                    </div>
-                                </div>
-                                <div class="col-md-2">
-                                    <div class="h4 pull-left">
-                                        <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-                                    </div>
-                                    <div class="h4 pull-left">
-                                        <?php echo $goal["rating"]; ?>
-                                    </div>
-                                </div>
-                                <div class="col-md-8"></div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="h3">Goals</div>
                             </div>
-                            <div class="row vertical-center">
-                                <div class="col-md-12">
-                                    <div class="progress">
-                                        <div class="progress-bar" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $goal["progress"]; ?>%;">
-                                        <div>
-                                            <?php echo $goal["totalDuration"]; ?>/<?php echo $goal["targetDuration"]; ?> hours
-                                        </div>
-                                        </div>
-                                    </div>
-                                    
+                            <div class="col-md-6">
+                                <div class="h3 pull-right">
+                                    <form action="/oztinate_dev/teacher/goals">
+                                        <input class="hidden" name="userId" id="userId" value="<?php echo $data["userId"]; ?>">
+                                        <button type="submit" class="btn">
+                                            <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+                                        </button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <!-- <div class="panel-body"> -->
+                        <ul class="list-group">
+                            <?php foreach ($data["goals"] as $goal) {?>
+                                <li class="list-group-item">
+                                    <div class="row">
+                                        <div class="col-md-2">
+                                            <div class="h4 pull-left">
+                                                <span class="glyphicon glyphicon-tag" aria-hidden="true"></span>
+                                            </div>
+                                            <div class="h4 pull-left">
+                                                <?php echo $goal["name"]; ?>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-2">
+                                            <div class="h4 pull-left">
+                                                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                            </div>
+                                            <div class="h4 pull-left">
+                                                <?php echo $goal["rating"]; ?>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-8"></div>
+                                    </div>
+                                    <div class="row vertical-center">
+                                        <div class="col-md-12">
+                                            <div class="progress">
+                                                <div class="progress-bar" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $goal["progress"]; ?>%;">
+                                                    <div>
+                                                        <?php echo $goal["totalDuration"]; ?>/<?php echo $goal["targetDuration"]; ?> hours
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    <!-- </div> -->
                 </div>
-                <?php } ?>
-                <div class="row">
-                    <div class="col-md-9">
-                        <div class="h3 pull-left">Exercises</div>
+
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="h3">Exercises</div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="h3 pull-right">
+                                    <form action="/oztinate_dev/teacher/assign_exercise">
+                                        <input class="hidden" name="userId" id="userId" value="<?php echo $data["userId"]; ?>">
+                                        <button type="submit" class="btn">
+                                            <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-md-3">
-                        <form action="/oztinate_dev/teacher/assign_exercise">
-                            <input class="hidden" name="userId" id="userId" value="<?php echo $data["userId"]; ?>">
-                            <button type="submit" class="btn btn-primary btn-block">Assign Exercise</button>
-                        </form>
-                    </div>
-                </div>
-                    <div class="row">
-                    <!-- <div class="col-md-1"></div> -->
-                    <div class="col-md-12">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th class="col-md-6">Name</th>
-                                    <th class="col-md-4">Status</th>
-                                    <th class="col-md-2"></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach ($data["assignedTasks"] as $task) {?>
-                                <tr>
-                                    <td class="col-md-6">
-                                        <div class="h5">
-                                            <?php echo $task["taskName"]; ?>
-                                        </div>
-                                    </td>
-                                    <td class="col-md-4">
-                                        <div class="h5">
-                                            <?php echo $task["status"]; ?>
-                                        </div>
-                                    </td>
-                                    <td class="col-md-2">
-                                        <div class="pull-right">
-                                            <form action="/oztinate_dev/teacher/review_exercise">
-                                                <input class="hidden" name="taskId" id="taskId" value="<?php echo $task["id"]; ?>">
-                                                    <button type="submit" class="btn">
-                                                        <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-                                                    </button>
-                                            </form>
-                                        </div>
-                                    </td>
-                                <?php } ?>
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- <div class="col-md-1"></div> -->
+                    <ul class="list-group">
+                        <?php foreach ($data["assignedTasks"] as $task) {?>
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="h5">
+                                        <?php echo $task["taskName"]; ?>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="h5">
+                                        <?php echo $task["status"]; ?>
+                                    </div>
+                                </div>
+                                <div class="col-md-2">
+                                    <div class="pull-right">
+                                        <form action="/oztinate_dev/teacher/review_exercise">
+                                            <input class="hidden" name="taskId" id="taskId" value="<?php echo $task["id"]; ?>">
+                                            <button type="submit" class="btn">
+                                                <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+                                            </button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <?php } ?>
+                    </ul>
                 </div>
             </div>
             <div class="col-md-3">
