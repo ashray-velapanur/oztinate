d4856176650cc4adca0df0d586c457aba3de92bd
ashray <ashray.velapanur@gmail.com>
2017-01-14 09:19:40 +0530

    clean up - base template

--- pages/teacher/base.php
+++ pages/teacher/base.php
@@ -1,13 +1,34 @@
+<?php //include("src/functions.php");
+$basePath="http://".$_SERVER['HTTP_HOST']. '/oztinate_dev/';
+$basepath_admin = $basePath."admin/";
+?>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link href="./bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="./bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
-    <link href="./dist/css/sb-admin-2.css" rel="stylesheet">
-    <link href="./bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="./assets/css/navbar.css" rel="stylesheet" type="text/css">
-    <link href="./assets/css/dashboard.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo $basePath ?>bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="<?php echo $basePath ?>bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="<?php echo $basePath ?>bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Timeline CSS -->
+    <link href="<?php echo $basePath ?>dist/css/timeline.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="<?php echo $basePath ?>dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="<?php echo $basePath ?>bower_components/morrisjs/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="<?php echo $basePath ?>bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <link href="<?php echo $basePath ?>assets/css/dashboard.css" rel="stylesheet" type="text/css">
+
+    <link href="<?php echo $basePath ?>assets/css/navbar.css" rel="stylesheet" type="text/css">
 </head>
