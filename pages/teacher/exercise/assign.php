2f1807b56d2ae2951f04cad992562d7777dd712c
ashray <ashray.velapanur@gmail.com>
2017-01-14 20:44:18 +0530

    clean up - assign exercise

--- pages/teacher/exercise/assign.php
+++ pages/teacher/exercise/assign.php
@@ -57,10 +57,9 @@
             </div>
         </div>
     </div>
-    <script src="../bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="../bower_components/metisMenu/dist/metisMenu.min.js"></script>
-    <script src="../dist/js/sb-admin-2.js"></script>
+
+    <?php include(ROOT_DIR."/pages/teacher/scripts.php") ?>
+
     <script type="text/javascript">
         function updateTemplate(e) {
             var id = e.options[e.selectedIndex].value;
---
+++ pages/teacher/scripts.php
@@ -0,0 +1,9 @@
+<?php //include("src/functions.php");
+$basePath="http://".$_SERVER['HTTP_HOST']. '/oztinate_dev/';
+$basepath_admin = $basePath."admin/";
+?>
+
+<script src="<?php echo $basePath ?>bower_components/jquery/dist/jquery.min.js"></script>
+<script src="<?php echo $basePath ?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?php echo $basePath ?>bower_components/metisMenu/dist/metisMenu.min.js"></script>
+<script src="<?php echo $basePath ?>/dist/js/sb-admin-2.js"></script>
